4694b0fbf77dacd092adca0e4a38a460485829ea
Jennyheera <jennyheera@Jennyheeraui-iMac.local>
2026-01-17 16:39:02 +0900

    fix: hashrouter + github pages routing

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,17 @@ import Home from './pages/home';
 import Animation from './pages/animation/Animation';
 import Movie from './pages/movie/Movie';
 
-const App = () => {
+function App() {
     return (
-        <HashRouter>
-            <div id="wrap">
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/animation" element={<Animation />} />
-                    <Route path="/movie" element={<Movie />} />
-
-                    {/* ⭐️ 이 줄 추가 */}
-                    <Route path="*" element={<Home />} />
-                </Routes>
-            </div>
+        <HashRouter basename="/myvacation">
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/animation" element={<Animation />} />
+                <Route path="/movie" element={<Movie />} />
+                <Route path="*" element={<Home />} />
+            </Routes>
         </HashRouter>
     );
-};
+}
 
 export default App;
