750161c10da0a12848c1cf1d49fdf94f7857f387
Jennyheera <jennyheera@Jennyheeraui-iMac.local>
2026-01-17 15:23:19 +0900

    fix: use HashRouter for github pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,20 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 
 import Animation from './pages/animation/Animation';
 import Movie from './pages/movie/Movie';
 import Home from './pages/home';
+
 const App = () => {
     return (
-        <BrowserRouter>
+        <HashRouter>
             <div id="wrap">
                 <Routes>
-                    {/* 화면에 보이는영역 */}
                     <Route path="/" element={<Home />} />
                     <Route path="/animation" element={<Animation />} />
                     <Route path="/movie" element={<Movie />} />
                 </Routes>
             </div>
-        </BrowserRouter>
+        </HashRouter>
     );
 };
 
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-    base: './', // master 루트에서 배포할 때
+    base: './',
     plugins: [react()],
 });
